9b69b116841dd6167cf717abb23969fd93ac6e3e
alim <7475149@mail.ru>
2022-08-31 19:34:52 +0600

    решена 1 задача

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+Console.WriteLine("введите число А");                        // сообщение о вводе числа
+int numA = Convert.ToInt32(Console.ReadLine());              // присваивание числу numА введенного значения
+Console.WriteLine("введите степень B");                      // сообщение о вводе степени
+int numB = Convert.ToInt32(Console.ReadLine());              // присваивание переменной numB введенного значения
+
+double rezultat = Math.Pow(numA, numB);                      // собственно само решение через метод Math.Pow uly имеется 2 аргумента первый число второй степень
+Console.WriteLine("Результат - "+rezultat);                  // вывод  результата
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("введите число ");
+int mun = Convert.ToInt32(Console.ReadLine());
+
